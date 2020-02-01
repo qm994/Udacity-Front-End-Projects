61164b30a0d25073dc4995d05ba6eecbdf114bad
Qingyuan Ma <qm28@georgetown.edu>
2020-02-01 11:58:27 -0500

    export js to library

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -5,3 +5,10 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+// export the js functions to the Client library, after export to Client library,
+// make sure to add the `Client.` in front of all the js functions
+export {
+    checkForName,
+    handleSubmit
+}

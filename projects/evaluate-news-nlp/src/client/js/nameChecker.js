2c8bd6da50401ddd3b955f4d4aacc8d76f40f5cc
Qingyuan Ma <qm28@georgetown.edu>
2020-02-08 12:30:04 -0500

    finally successfully update the information in the UI

--- projects/evaluate-news-nlp/src/client/js/nameChecker.js
+++ projects/evaluate-news-nlp/src/client/js/nameChecker.js
@@ -1,16 +1,19 @@
-function checkForName(inputText) {
+async function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
 
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
-    }
+    //post the input text url to the server then 
+    //send to the Aylien API
+    //https://www.healthline.com/nutrition/10-proven-benefits-of-cinnamon
+    const response = await fetch("http://localhost:8080/api", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(inputText)
+    })
+
+    return await response.json();
 }
 
 export { checkForName }
--- projects/evaluate-news-nlp/src/server/mockAPI.js
+++ projects/evaluate-news-nlp/src/server/mockAPI.js
@@ -1,6 +1,6 @@
 let json = {
     'title': 'test json response',
-    'message': 'this is a message',
+    'message': 'This is the API response message',
     'time': 'now'
 }
 
